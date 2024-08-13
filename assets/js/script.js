46bb1a905e071408e870e95490edad74f610427a
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-08-13 10:21:42 +0000

    add function for generating and displaying random computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,40 @@ const displayCompterChoice = document.getElementById('computer-emoji');
 const displayPlayerChoice = document.getElementById('player-emoji');
 const displayResult = document.getElementById('results-message');
 let buttons = ['rock', 'paper', 'scissors'];
+let choice;
+let randomNumber;
+let computerChoice;
 
 // add event listeners for buttons
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     buttons = document.getElementsByTagName('button');
 
     for (let button of buttons) {
-        button.addEventListener('click', function() {
+        button.addEventListener('click', function () {
             if (this.getAttribute('class') === 'btn');
             choice = this.textContent;
-            console.log(choice);
+            displayPlayerChoice.innerHTML = choice;
+            createComputerChoice();
         })
     }
 })
+
+/**
+ * Create a function for a random computer chioce
+ * Credit Ania Kobow - Code With Ania Kubow - 
+ * youTube
+ */
+function createComputerChoice() {
+    randomNumber = Math.floor(Math.random() * buttons.length);
+
+    if (randomNumber === 0) {
+        computerChoice = '👊'
+    }
+    if (randomNumber === 1) {
+        computerChoice = '🤚'
+    }
+    if (randomNumber === 2) {
+        computerChoice = '✌'
+    }
+    displayCompterChoice.innerHTML = computerChoice;
+}
