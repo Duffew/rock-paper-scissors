ec31f77f193954106d584c58efda32bcb33847db
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-08-13 10:11:05 +0000

    add event listeners for buttons; adjust css stying for results table

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,15 @@ const displayPlayerChoice = document.getElementById('player-emoji');
 const displayResult = document.getElementById('results-message');
 let buttons = ['rock', 'paper', 'scissors'];
 
+// add event listeners for buttons
+document.addEventListener("DOMContentLoaded", function() {
+    buttons = document.getElementsByTagName('button');
 
+    for (let button of buttons) {
+        button.addEventListener('click', function() {
+            if (this.getAttribute('class') === 'btn');
+            choice = this.textContent;
+            console.log(choice);
+        })
+    }
+})
