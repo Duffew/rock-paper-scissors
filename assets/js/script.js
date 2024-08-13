2c82cc5e640ebba3a41fbf94d8b7a6dd30710b5f
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-08-13 09:57:14 +0000

    declare initial variables for DOM elements

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/* declare variables for DOM elements
+Credit Ania Kobow - Code With Ania Kubow - 
+youTube */
+
+const displayCompterChoice = document.getElementById('computer-emoji');
+const displayPlayerChoice = document.getElementById('player-emoji');
+const displayResult = document.getElementById('results-message');
+let buttons = ['rock', 'paper', 'scissors'];
+
+
