bdbe80a27c23286915923c9f49e5590a7ea87b36
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-08-14 10:47:43 +0000

    fix : add missing array items to buttons variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ youTube */
 const displayCompterChoice = document.getElementById('computer-emoji');
 const displayPlayerChoice = document.getElementById('player-emoji');
 const displayResult = document.getElementById('results-message');
-let buttons = ['rock', 'paper', 'scissors'];
+let buttons = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
 let choice;
 let computerChoice;
 let resultMessage;
