7ac39efad53fc12dd0afe30d6ee03f8e8ac4131f
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-08-15 10:16:27 +0000

    feat: add logic and styling for Rules modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -99,3 +99,30 @@ function resetTally() {
     displayComputerChoice.innerText = "";
     displayPlayerChoice.innerText = "";
 }
+// Logic for the modal - acknowlegde w3schools.com library
+
+// Get the modal
+const modal = document.getElementById("rules-modal");
+
+// Get the button that opens the modal
+const rulesBtn = document.getElementById("rules");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+rulesBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
